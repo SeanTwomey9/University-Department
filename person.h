4a3052cd20599d2cfc72b560f95f9b767248c829
Sean Twomey <56229866+SeanTwomey9@users.noreply.github.com>
2021-01-30 16:18:47 -0500

    Adding person header file

---
+++ person.h
@@ -0,0 +1,14 @@
+//
+// Created by Sean Twomey on 1/30/21.
+//
+
+#ifndef HW3_PERSON_H
+#define HW3_PERSON_H
+
+/**
+ * Person class definition
+ * Reprenting a person with a ID and name
+ */
+
+
+#endif //HW3_PERSON_H
