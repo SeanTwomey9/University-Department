0531d847bf1b170a2e4c39dbf503100ccf8a3d24
Sean Twomey <56229866+SeanTwomey9@users.noreply.github.com>
2021-01-30 18:35:10 -0500

    Finished employee header class

--- employee.h
+++ employee.h
@@ -8,10 +8,27 @@
 #include "person.h" //person class definition
 using namespace std;
 
-class Employee {
+class Employee: public Person {
 
+public:
 
-};
+    Employee(double, const string&, int, const string&); //full employee constructor
+    Employee(); //empty employee constructor
+
+    void setSalary(double); //sets the employee's salary
+    double getSalary() const; //returns the employee's salary
+
+    void setDepartment(const string&); //sets the employee's department
+    string getDepartment() const; //returns the employee's department
+
+    void print() const; //output a employee representation
+
+private:
+
+    double salary; //employee's salary
+    string department; //employee's department
+
+};//end Employee class
 
 
 #endif //HW3_EMPLOYEE_H
