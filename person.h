239fa96d21c30838a11a4bed7d6e758cb402005b
Sean Twomey <56229866+SeanTwomey9@users.noreply.github.com>
2021-01-30 16:25:48 -0500

    Finished person header file

--- person.h
+++ person.h
@@ -10,5 +10,22 @@
  * Reprenting a person with a ID and name
  */
 
+class Person {
+public:
+    Person(int, string); //full constructor
+    Person(int = 0, string = ""); //empty constructor
+
+    void setID(int); //set person's ID
+    int getID() const; //return person's ID
+
+    void setName(string); //set person's name
+    string getName() const; //return person's name
+
+    void print() const; //outputs a Person object
+
+private:
+    int ID;
+    string name;
+}; //end class Person
 
 #endif //HW3_PERSON_H
